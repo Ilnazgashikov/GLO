0cb84a30495188a0d2714230b65803f6fb7c1161
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-05-13 22:54:53 +0300

    второе задание

---
+++ script.js
@@ -0,0 +1,19 @@
+let title = 'firstPeoject';
+let screens = "Простые, Cложные, Интерактивные";
+let screenPrice = 88;
+let rollback = 13;
+let fullPrice = 150000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(screenPrice+'$');
+console.log(fullPrice+'$');
+
+console.log(screens.toLocaleLowerCase().split(","));
+
+console.log(fullPrice*(rollback/100));
