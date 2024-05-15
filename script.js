fb06f210754fe37e00b0c3d7d1ed506672a3bd9d
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-05-15 23:18:32 +0300

    урок 3

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-<<<<<<< HEAD
-const title = 1;
-const screen = 1;
-const screenPrice = 3;
-const rollBack = 4;
-const fullPrice = 5;
-const adaptive = 6;
-alert("Hello world!");
-console.log("Hello");
-=======
-let title = 'firstPeoject';
-let screens = "Простые, Cложные, Интерактивные";
-let screenPrice = 88;
-let rollback = 13;
-let fullPrice = 150000;
-let adaptive = true;
+'use strict';
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+const screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+const rollback = 12;
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуг нужен?");
+const servicePrice1 = prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуг нужен?");
+const servicePrice2 = prompt("Сколько это будет стоить?");
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
 
-console.log(screens.length);
+switch (true) {
+    case fullPrice >= 30000: alert("Даем скидку в 10%");
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000: alert("Даем скидку в 5%");
+        break;
+    case fullPrice >= 0 && fullPrice < 15000: alert("Скидка не предусмотрена");
+    default: alert("Что то пошло не так");
+};
 
-console.log(screenPrice+'$');
-console.log(fullPrice+'$');
 
-console.log(screens.toLocaleLowerCase().split(","));
-
-console.log(fullPrice*(rollback/100));
->>>>>>> lesson2
+console.log(title);
+console.log(screens);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log("Откат посреднику", Math.floor(servicePercentPrice));
