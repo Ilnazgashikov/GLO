f00093e19c6abd7ed24ceff0e186eef9a1d70220
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-05-19 22:25:17 +0300

    урок 6

--- script.js
+++ script.js
@@ -1,95 +1,48 @@
 'use strict';
-let title;
-let screens;
-let screenPrice;
-let adaptive;
-let rollback = 12;
-let allServicePrices;
-let fullPrice;
-let servicePercentPrice;
-let service1;
-let service2;
-
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-};
-
-const asking = function () {
-    title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-    screens = prompt("Какие типы экранов нужно разработать?", "Простые, сложные, интерактивные");
-    screenPrice = prompt("Сколько будет стоить данная работа?");
-
-    do {
-      screenPrice = prompt("Сколько будет стоить данная работа?");
-    } while (!isNumber(screenPrice));
-
-    adaptive = confirm("Нужен ли адаптив на сайте?");
-};
-const getAllServicePrices = function () {
-    let sum = 0;
-    for (let i = 0; i < 2; i++) {
-      if (i === 0) {
-        service1 = prompt("Какой дополнительный тип услуги нужен?");
-      } else if (i === 1) {
-        service2 = prompt("Какой дополнительный тип услуги нужен?");
+function guessNumber() {
+  let countAttempts = 10;
+  function getRandomInt() {
+    return Math.floor(Math.random() * 100);
+  }
+  let randNum = getRandomInt();
+  alert("В этой игре вам необходимо угадать число в диапозоне от 1 до 100");
+  if (confirm("Начать игру?")) {
+    gameBody();
+  }
+  function gameBody() {
+    if (countAttempts > 0) {
+         let myNumber =+ prompt("Введите ваше число: ");
+    if (!isNaN(myNumber) && myNumber <= 100) {
+          if (randNum === myNumber) {
+         if (confirm("Поздравляю! Вы угадали загадочное число. Хотите попробовать еще?")) {
+             countAttempts = 10;
+             gameBody();
+         }
+        }
+        else if (randNum > myNumber) {
+           countAttempts--;
+           alert("Загадочное число больше вашего. У вас осталось " + countAttempts + " попыток. Попробуйте еще раз");
+           gameBody();
+        }
+        else if (randNum < myNumber) {
+           countAttempts--;
+           alert("Загадочное число меньше вашего. У вас осталось " + countAttempts + " попыток. Попробуйте еще раз");
+           gameBody();
+        }
       }
-      const numberChecks = () => {
-        let n = 0;
-        do {
-          n = +prompt("Сколько это будет стоить?");
-        } while (!isNumber(n));
-        return +n;
-      };
-      sum += numberChecks();
+      else {
+       alert("Введите корректное число");
+       gameBody();
+      } 
     }
-    return sum;
-  };
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
-};
-
-const getFullPrice = function () {
-    return screenPrice + allServicePrices;
-};
-
-const getServicePercentPrice = function () {
-    return fullPrice - (fullPrice * (rollback / 100));
-};
-
-const getTitle = function () {
-    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLocaleLowerCase();
-};
-
-const getRollbackMessage = function (price) {
-    if (price >= 30000) {
-        return "Даем скидку в 10%";
-    } else if (price >= 15000 && price < 30000) {
-        return "Даем скидку в 5%";
-    } else if (price >= 0 && price < 15000) {
-        return "Скидка не предусмотрена";
-    } else {
-        return "Что то пошло не так";
-    };
-};
-
-asking();
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice();
-servicePercentPrice = getServicePercentPrice();
-title = getTitle();
-
-showTypeOf(title);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
-
-console.log("allServicePrices", allServicePrices);
-
-console.log(getRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-console.log(servicePercentPrice);
-
-console.log("Стоимость верстки экранов" + screenPrice + "рубли и Стоимость разработки сайта" + fullPrice + "рубли");
+    else {
+       if (confirm("Вы проиграли. Попробуйте еще раз?")) {
+         guessNumber(); 
+       }
+       else {
+        return null;
+       }
+    }
+   }
+}
+  guessNumber();
