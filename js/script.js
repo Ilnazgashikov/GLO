e92befa2511807d335ed71b05d9918fa3b7204a7
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-05-23 22:53:53 +0300

    урок 9 исправил

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 "use strict";
-const h1 = document.getElementsByTagName('h1')
+const h1 = document.getElementsByTagName('h1')[0];
 const handlerBtn = document.getElementsByClassName('handler_btn');
 const screenBtn = document.querySelector('.screen-btn');
 const otherItems1 = document.querySelectorAll('.other-items.percent');
 const otherItems2 = document.querySelectorAll('.other-items.number');
 const rollbackInput = document.querySelector('.rollback input');
 const rollbackSpan = document.querySelector('.rollback span');
-const totalInput = document.getElementsByClassName('total-input')
-
+const total = document.getElementsByClassName('total-input')[0];
+const total_count = document.getElementsByClassName('total-input')[1];
+const total_count_other = document.getElementsByClassName('total-input')[2];
+const total_full_count = document.getElementsByClassName('total-input')[3];
+const total_count_rollback = document.getElementsByClassName('total-input')[4];
 let screen = document.querySelectorAll('.screen');
 const calculate = handlerBtn[0];
 const reset = handlerBtn[1];
 
-Array.from(totalInput).forEach((el) => {
-    console.log(el);
-});
-
-console.log(h1[0]);
+console.log(total);
+console.log(total_count);
+console.log(total_count_other);
+console.log(total_full_count);
+console.log(total_count_rollback);
+console.log(h1);
 console.log(calculate);
 console.log(reset);
 console.log(screenBtn);
